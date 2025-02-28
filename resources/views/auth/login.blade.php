709bf5e3134e1349e0dd0a714b6cfa78ab3e36e9
Hussains3 <hussainasabbir3@gmail.com>
2025-02-28 14:19:48 +0600

    Working on login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,56 @@
 <x-guest-layout>
     <!-- Session Status -->
-    <section class="page-content bg-cover bg-center bg-no-repeat bg-fixed min-h-screen flex justify-center items-center" style="background-image: url('{{ asset('images/background.jpg') }}');">
-        <div class="">
+    <section class="min-h-screen flex justify-between bg-no-repeat bg-center bg-cover" style="background-image: url('{{ asset('images/background.jpg') }}');">
+        <div class="bg-white min-h-full flex justify-center items-center">
+            <div class="w-full sm:w-7xl mt-6 px-6 py-4 overflow-hidden sm:rounded-lg">
+                <x-auth-session-status class="mb-4" :status="session('status')" />
+
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <!-- Email Address -->
+                    <div>
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <!-- Password -->
+                    <div class="mt-4">
+                        <x-input-label for="password" :value="__('Password')" />
+
+                        <x-text-input id="password" class="block mt-1 w-full"
+                                        type="password"
+                                        name="password"
+                                        required autocomplete="current-password" />
+
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+
+                    <!-- Remember Me -->
+                    <div class="block mt-4">
+                        <label for="remember_me" class="inline-flex items-center">
+                            <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+                            <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                        </label>
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+                        @if (Route::has('password.request'))
+                            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                                {{ __('Forgot your password?') }}
+                            </a>
+                        @endif
+
+                        <x-primary-button class="ms-3">
+                            {{ __('Log in') }}
+                        </x-primary-button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    
+        <div class="bg-red-500 h-full hidden" style="background-image: url('{{ asset('images/background.jpg') }}');">
 
             <div>
                 <a href="/">

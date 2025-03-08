ba74551a9dba59e1655a41a83ba984a7ddab8bb3
Hussains3 <hussainasabbir3@gmail.com>
2025-03-08 13:47:30 +0600

    Home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,76 +5,113 @@
         <x-hero></x-hero>
 
 
+        {{-- Notice Board --}}
+        <section class=" pb-10 pt-16">
+            <div class="max-w-7xl mx-auto flex gap-4">
+                <div class="flex-grow">
+
+                    <h2 class="text-3xl font-bold text-left mb-8 text-gray-800">Notice Board</h2>
+
+                    <div class="flex flex-col gap-4">
+                        {{-- Notice --}}
+                        <p class="card p-6 shadow-md flex gap-4 justify-start items-center rounded-lg bg-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg"> > এতদ্বারা বেনাপোল কাস্টমস সি এন্ড এফ এজেন্টস এসোসিয়েশনের সকল সম্মানিত সদস্যগণের সদয় অবগতির জন্য জানানো যাচ্ছে যে,
+                        </p>
+                        <p class="card p-6 shadow-md flex gap-4 justify-start items-center rounded-lg bg-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg"> > এতদ্বারা বেনাপোল কাস্টমস সি এন্ড এফ এজেন্টস এসোসিয়েশনের সকল সম্মানিত সদস্যগণের সদয় অবগতির জন্য জানানো যাচ্ছে যে,
+                        </p>
+                        <p class="card p-6 shadow-md flex gap-4 justify-start items-center rounded-lg bg-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg"> > এতদ্বারা বেনাপোল কাস্টমস সি এন্ড এফ এজেন্টস এসোসিয়েশনের সকল সম্মানিত সদস্যগণের সদয় অবগতির জন্য জানানো যাচ্ছে যে,
+                        </p>
+                        <p class="card p-6 shadow-md flex gap-4 justify-start items-center rounded-lg bg-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg"> > এতদ্বারা বেনাপোল কাস্টমস সি এন্ড এফ এজেন্টস এসোসিয়েশনের সকল সম্মানিত সদস্যগণের সদয় অবগতির জন্য জানানো যাচ্ছে যে,
+                        </p>
+
+                    </div>
+                </div>
+                <div class="card border-l border-indigo-500 px-2 w-40">
+                    <a class="block text-center px-4 py-2 border-b border-gray-200 rounded-md shadow-md hover:shadow-lg hover:scale-110 duration-150 transition-all w-full" href="{{route('notices.index')}}">Notices</a>
+                    <a class="block text-center px-4 py-2 border-b border-gray-200 rounded-md shadow-md hover:shadow-lg hover:scale-110 duration-150 transition-all w-full" href="{{route('notices.index')}}">Notices</a>
+                    <a class="block text-center px-4 py-2 border-b border-gray-200 rounded-md shadow-md hover:shadow-lg hover:scale-110 duration-150 transition-all w-full" href="{{route('notices.index')}}">Notices</a>
+                    <a class="block text-center px-4 py-2 border-b border-gray-200 rounded-md shadow-md hover:shadow-lg hover:scale-110 duration-150 transition-all w-full" href="{{route('notices.index')}}">Notices</a>
+                    <a class="block text-center px-4 py-2 border-b border-gray-200 rounded-md shadow-md hover:shadow-lg hover:scale-110 duration-150 transition-all w-full" href="{{route('notices.index')}}">Notices</a>
+                    <a class="block text-center px-4 py-2 border-b border-gray-200 rounded-md shadow-md hover:shadow-lg hover:scale-110 duration-150 transition-all w-full" href="{{route('notices.index')}}">Notices</a>
+                    <a class="block text-center px-4 py-2 border-b border-gray-200 rounded-md shadow-md hover:shadow-lg hover:scale-110 duration-150 transition-all w-full" href="{{route('notices.index')}}">Notices</a>
+                    <a class="block text-center px-4 py-2 border-b border-gray-200 rounded-md shadow-md hover:shadow-lg hover:scale-110 duration-150 transition-all w-full" href="{{route('notices.index')}}">Notices</a>
+                </div>
+            </div>
+        </section>
+
+
         {{-- Advisory Committee --}}
         <section class=" py-10">
-            <div class="max-w-7xl mx-auto">
-                <h2 class="text-3xl font-bold text-center mb-8 text-gray-800">Advisory Committee</h2>
+            <div class="max-w-7xl mx-auto flex gap-4">
+                <div class="flex-grow">
 
-                <div class="grid grid-cols-2 sm:grid-cols-3 gap-6 mt-5">
-                    {{-- Member --}}
-                    <div class="card p-6 shadow-md flex gap-4 justify-start items-center rounded-lg bg-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
-                        {{-- image --}}
-                        <div class="w-20 h-20 rounded-full bg-cover bg-no-repeat" style="background-image: url({{ asset('images/placeholder.jpg') }})"></div>
-                        <div>
-                            {{-- name --}}
-                            <h3 class="font-bold text-lg text-gray-900">Sabbir Hussain</h3>
-                            {{-- designation --}}
-                            <p class="text-gray-600">Developer</p>
+                    <h2 class="text-3xl font-bold text-center mb-8 text-gray-800">Advisory Committee</h2>
+
+                    <div class="grid grid-cols-2 sm:grid-cols-3 gap-6 mt-5">
+                        {{-- Member --}}
+                        <div class="card p-6 shadow-md flex gap-4 justify-start items-center rounded-lg bg-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+                            {{-- image --}}
+                            <div class="w-20 h-20 rounded-full bg-cover bg-no-repeat" style="background-image: url({{ asset('images/placeholder.jpg') }})"></div>
+                            <div>
+                                {{-- name --}}
+                                <h3 class="font-bold text-lg text-gray-900">Sabbir Hussain</h3>
+                                {{-- designation --}}
+                                <p class="text-gray-600">Developer</p>
+                            </div>
                         </div>
-                    </div>
-                    {{-- Repeat the above block for other members --}}
-                    <div class="card p-6 shadow-md flex gap-4 justify-start items-center rounded-lg bg-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
-                        {{-- image --}}
-                        <div class="w-20 h-20 rounded-full bg-cover bg-no-repeat" style="background-image: url({{ asset('images/placeholder.jpg') }})"></div>
-                        <div>
-                            {{-- name --}}
-                            <h3 class="font-bold text-lg text-gray-900">Sabbir Hussain</h3>
-                            {{-- designation --}}
-                            <p class="text-gray-600">Developer</p>
+                        {{-- Repeat the above block for other members --}}
+                        <div class="card p-6 shadow-md flex gap-4 justify-start items-center rounded-lg bg-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+                            {{-- image --}}
+                            <div class="w-20 h-20 rounded-full bg-cover bg-no-repeat" style="background-image: url({{ asset('images/placeholder.jpg') }})"></div>
+                            <div>
+                                {{-- name --}}
+                                <h3 class="font-bold text-lg text-gray-900">Sabbir Hussain</h3>
+                                {{-- designation --}}
+                                <p class="text-gray-600">Developer</p>
+                            </div>
                         </div>
-                    </div>
-                    {{-- Repeat the above block for other members --}}
-                    <div class="card p-6 shadow-md flex gap-4 justify-start items-center rounded-lg bg-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
-                        {{-- image --}}
-                        <div class="w-20 h-20 rounded-full bg-cover bg-no-repeat" style="background-image: url({{ asset('images/placeholder.jpg') }})"></div>
-                        <div>
-                            {{-- name --}}
-                            <h3 class="font-bold text-lg text-gray-900">Sabbir Hussain</h3>
-                            {{-- designation --}}
-                            <p class="text-gray-600">Developer</p>
+                        {{-- Repeat the above block for other members --}}
+                        <div class="card p-6 shadow-md flex gap-4 justify-start items-center rounded-lg bg-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+                            {{-- image --}}
+                            <div class="w-20 h-20 rounded-full bg-cover bg-no-repeat" style="background-image: url({{ asset('images/placeholder.jpg') }})"></div>
+                            <div>
+                                {{-- name --}}
+                                <h3 class="font-bold text-lg text-gray-900">Sabbir Hussain</h3>
+                                {{-- designation --}}
+                                <p class="text-gray-600">Developer</p>
+                            </div>
                         </div>
-                    </div>
-                    {{-- Repeat the above block for other members --}}
-                    <div class="card p-6 shadow-md flex gap-4 justify-start items-center rounded-lg bg-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
-                        {{-- image --}}
-                        <div class="w-20 h-20 rounded-full bg-cover bg-no-repeat" style="background-image: url({{ asset('images/placeholder.jpg') }})"></div>
-                        <div>
-                            {{-- name --}}
-                            <h3 class="font-bold text-lg text-gray-900">Sabbir Hussain</h3>
-                            {{-- designation --}}
-                            <p class="text-gray-600">Developer</p>
+                        {{-- Repeat the above block for other members --}}
+                        <div class="card p-6 shadow-md flex gap-4 justify-start items-center rounded-lg bg-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+                            {{-- image --}}
+                            <div class="w-20 h-20 rounded-full bg-cover bg-no-repeat" style="background-image: url({{ asset('images/placeholder.jpg') }})"></div>
+                            <div>
+                                {{-- name --}}
+                                <h3 class="font-bold text-lg text-gray-900">Sabbir Hussain</h3>
+                                {{-- designation --}}
+                                <p class="text-gray-600">Developer</p>
+                            </div>
                         </div>
-                    </div>
-                    {{-- Repeat the above block for other members --}}
-                    <div class="card p-6 shadow-md flex gap-4 justify-start items-center rounded-lg bg-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
-                        {{-- image --}}
-                        <div class="w-20 h-20 rounded-full bg-cover bg-no-repeat" style="background-image: url({{ asset('images/placeholder.jpg') }})"></div>
-                        <div>
-                            {{-- name --}}
-                            <h3 class="font-bold text-lg text-gray-900">Sabbir Hussain</h3>
-                            {{-- designation --}}
-                            <p class="text-gray-600">Developer</p>
+                        {{-- Repeat the above block for other members --}}
+                        <div class="card p-6 shadow-md flex gap-4 justify-start items-center rounded-lg bg-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+                            {{-- image --}}
+                            <div class="w-20 h-20 rounded-full bg-cover bg-no-repeat" style="background-image: url({{ asset('images/placeholder.jpg') }})"></div>
+                            <div>
+                                {{-- name --}}
+                                <h3 class="font-bold text-lg text-gray-900">Sabbir Hussain</h3>
+                                {{-- designation --}}
+                                <p class="text-gray-600">Developer</p>
+                            </div>
                         </div>
-                    </div>
-                    {{-- Repeat the above block for other members --}}
-                    <div class="card p-6 shadow-md flex gap-4 justify-start items-center rounded-lg bg-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
-                        {{-- image --}}
-                        <div class="w-20 h-20 rounded-full bg-cover bg-no-repeat" style="background-image: url({{ asset('images/placeholder.jpg') }})"></div>
-                        <div>
-                            {{-- name --}}
-                            <h3 class="font-bold text-lg text-gray-900">Sabbir Hussain</h3>
-                            {{-- designation --}}
-                            <p class="text-gray-600">Developer</p>
+                        {{-- Repeat the above block for other members --}}
+                        <div class="card p-6 shadow-md flex gap-4 justify-start items-center rounded-lg bg-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+                            {{-- image --}}
+                            <div class="w-20 h-20 rounded-full bg-cover bg-no-repeat" style="background-image: url({{ asset('images/placeholder.jpg') }})"></div>
+                            <div>
+                                {{-- name --}}
+                                <h3 class="font-bold text-lg text-gray-900">Sabbir Hussain</h3>
+                                {{-- designation --}}
+                                <p class="text-gray-600">Developer</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -83,39 +120,44 @@
 
         {{-- Contact us form --}}
         <section class="bg-gray-100 py-10">
-            <div class="max-w-3xl mx-auto">
-                <h2 class="text-3xl font-bold text-center mb-8 text-gray-800">Contact Us</h2>
-                <div class="container mx-auto px-4 py-8">
-                    <form action="#" method="POST" class="grid grid-cols-1 gap-6">
-                        <div>
-                            <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
-                            <input type="text" name="name" id="name" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm
-                            @error('name') border-red-500 @enderror" value="{{ old('name') }}">
-                            @error('name')
-                                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                            @enderror
-                        </div>
-                        <div>
-                            <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-                            <input type="email" name="email" id="email" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm
-                            @error('email') border-red-500 @enderror" value="{{ old('email') }}">
-                            @error('email')
-                                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                            @enderror
-                        </div>
-                        <div>
-                            <label for="message" class="block text-sm font-medium text-gray-700">Message</label>
-                            <textarea name="message" id="message" rows="4" class="mt-1 block
-                            w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm
-                            @error('message') border-red-500 @enderror">{{ old('message') }}</textarea>
-                            @error('message')
-                                <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                            @enderror
-                        </div>
-                        <div>
-                            <button type="submit" class="w-full py-2 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Send</button>
-                        </div>
-                    </form>
+            <div class="max-w-7xl mx-auto">
+                <h2 class="text-3xl font-bold text-left mb-8 text-gray-800">Contact Us</h2>
+                <div class="flex justify-between gap-4">
+                    <div class="basis-1/2">
+                        <p>You can send us a message using this form, <br/> or contact us by email or phone.</p>
+                    </div>
+                    <div class="px-4 py-8 basis-1/2">
+                        <form action="#" method="POST" class="grid grid-cols-1 gap-6">
+                            <div>
+                                <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                                <input type="text" name="name" id="name" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm
+                                @error('name') border-red-500 @enderror" value="{{ old('name') }}">
+                                @error('name')
+                                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                                @enderror
+                            </div>
+                            <div>
+                                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                                <input type="email" name="email" id="email" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm
+                                @error('email') border-red-500 @enderror" value="{{ old('email') }}">
+                                @error('email')
+                                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                                @enderror
+                            </div>
+                            <div>
+                                <label for="message" class="block text-sm font-medium text-gray-700">Message</label>
+                                <textarea name="message" id="message" rows="4" class="mt-1 block
+                                w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm
+                                @error('message') border-red-500 @enderror">{{ old('message') }}</textarea>
+                                @error('message')
+                                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                                @enderror
+                            </div>
+                            <div>
+                                <button type="submit" class="w-full py-2 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Send</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </section>
